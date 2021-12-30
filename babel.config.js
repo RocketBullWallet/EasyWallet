42070137b8d01632638ba618ecfa29078d70209f
Ilton Silveira <iltonsilveira91@gmail.com>
2021-12-30 20:43:09 -0300

    shell script helpers added & assets folder moved & more config files added (buckconfig for android, nvm versionating)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+
+const envLine = fs
+  .readFileSync('./.env', 'utf8')
+  .split('\n')
+  .find(l => l.startsWith('SCRIPT_NM='));
+const data = envLine && envLine.slice(10);
+
+function getAliasesFromTsConfig() {
+  const tsConfig = require('./tsconfig.json');
+  const paths = tsConfig.compilerOptions.paths;
+  let alias = {};
+  Object.keys(paths).forEach(key => {
+    alias[key] = `./${paths[key][0]}`;
+  });
+
+  return alias;
+}
+
 module.exports = function(api) {
   api.cache(true);
   return {
